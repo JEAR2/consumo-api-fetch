3c813c745a16a4c83ac4d9e014013edd83fd6f2c
JEAR2 <jhedacro@gmail.com>
2022-06-15 12:22:27 -0500

    Se agrega vista con bootstrap-4

--- js/struct.js
+++ js/struct.js
@@ -4,15 +4,27 @@ inputName.type="text";
 inputName.name="nombre";
 inputName.placeholder = "Nombre";
 inputName.required=true;
+inputName.classList.add("form-control");
 
 const inputConstellation = document.createElement("input");
 inputConstellation.type="text";
 inputConstellation.name="constelacion";
-inputConstellation.placeholder = "constelación";
+inputConstellation.placeholder = "Constelación";
 inputConstellation.required = true;
+inputConstellation.classList.add("form-control");
+
+const divName = document.createElement("div");
+divName.classList.add("col");
+divName.appendChild(inputName);
+
+const divConstellation = document.createElement("div");
+divConstellation.classList.add("col");
+divConstellation.appendChild(inputConstellation);
 
 const inputSend = document.createElement("input");
 inputSend.type = "submit";
+inputSend.classList.add("btn");
+inputSend.classList.add("btn-success");
 inputSend.value = "Enviar";
 
 const inputHidden = document.createElement("input");
@@ -21,6 +33,7 @@ inputHidden.name = "id";
 
 const form = document.createElement("form");
 form.classList.add("crud-form");
+form.classList.add("form-row");
 
 const h2Form = document.createElement("h2");
 h2Form.classList.add("crud-title");
@@ -30,10 +43,16 @@ const br = document.createElement("br");
 const brTwo = document.createElement("br");
 
 const articleOne = document.createElement("article");
+articleOne.classList.add("p-3");
+articleOne.classList.add("my-3");
+articleOne.classList.add("border");
 
 const articleTwo = document.createElement("article");
+articleTwo.classList.add("p-3");
+articleTwo.classList.add("my-3");
+articleTwo.classList.add("border");
 
-form.append(inputName,br,inputConstellation,brTwo,inputSend,inputHidden);
+form.append(divName,br,divConstellation,brTwo,inputSend,inputHidden);
 
 articleOne.append(h2Form,form);
 
@@ -47,12 +66,16 @@ thActions.textContent = "Acciones";
 const tr = document.createElement("tr");
 
 const thead = document.createElement("thead");
+thead.classList.add("thead-dark");
 
 const table = document.createElement("table");
 table.classList.add("crud-table");
+table.classList.add("table");
+table.classList.add("table-striped");
+
 
 const h2Table = document.createElement("h2");
-h2Table.textContent = "ver Santos";
+h2Table.textContent = "Listado de  Santos";
 
 const tbody = document.createElement("tbody");
 
@@ -80,6 +103,8 @@ buttonEdit.classList.add("edit");
 buttonEdit.textContent="Editar";
 const buttonDelete = document.createElement("button");
 buttonDelete.classList.add("delete");
+buttonDelete.classList.add("btn");
+buttonDelete.classList.add("btn-danger");
 buttonDelete.textContent = "Eliminar";
 tdActionsTemplate.append(buttonEdit,buttonDelete);
 
